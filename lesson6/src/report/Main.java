83ab82917bcbc2c2cecfd416abfc0c94472d5932
Sergiu1987 <sergiu.siscovschi@gmail.com>
2017-11-08 22:24:22 +0200

    Lectia 6 Posta Moldovei

---
+++ lesson6/src/report/Main.java
@@ -0,0 +1,78 @@
+package report;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    private static List<Report> report;
+    private static String trackingNumber;
+
+
+    public static void main(String[] args) throws IOException {
+        report = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert tracking number: ");
+        trackingNumber = scanner.nextLine();
+
+        String defaultURL = "http://www.posta.md/en/tracking?id=RB295349616SG";/*RS916319658NL*/
+
+        int indexTrackingNumber = defaultURL.indexOf("=");
+        String defaultTrackingNumber = defaultURL.substring(indexTrackingNumber + 1);
+        String url = defaultURL.replace(defaultTrackingNumber, trackingNumber);
+
+        URL postaMoldovei = new URL(url);
+        URLConnection connection = postaMoldovei.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+
+            if(inputLine.contains("row clearfix")) {
+
+                inputLine = in.readLine();
+                String date = parseLine(inputLine);
+
+                inputLine = in.readLine();
+                String country = parseLine(inputLine);
+
+                inputLine = in.readLine();
+                String location = parseLine(inputLine);
+
+                inputLine = in.readLine();
+                String event = parseLine(inputLine);
+
+                inputLine = in.readLine();
+                String extra = parseLine(inputLine);
+
+            report.add(new Report(date,country,location,event,extra));
+
+            }
+        }
+        System.out.println(report);
+        in.close();
+
+
+    }
+
+    private static String parseLine(String inputLine) {
+        String result;
+
+        int startIndex = inputLine.indexOf(">");
+        int endIndex = inputLine.lastIndexOf("<");
+        result = inputLine.substring(startIndex + 1,endIndex);
+
+        return result;
+
+
+    }
+}
---
+++ lesson6/src/report/Report.java
@@ -0,0 +1,72 @@
+package report;
+
+public class Report {
+    private String date;
+    private String country;
+    private String location;
+
+    @Override
+    public String toString() {
+        return " | " +
+                " |" + date + '\'' +
+                ", country='" + country + '\'' +
+                ", location='" + location + '\'' +
+                ", event='" + event + '\'' +
+                ", extra='" + extra + '\'' +
+                "}\n";
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    public String getDate() {
+
+        return date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    private String event;
+
+    public Report(String date, String country, String location, String event, String extra) {
+        this.date = date;
+        this.country = country;
+        this.location = location;
+        this.event = event;
+        this.extra = extra;
+    }
+
+    private String extra;
+
+}
