44991dd6ae4b7c4df24b5322083994f9fd424404
Sergiu1987 <sergiu.siscovschi@gmail.com>
2017-11-13 19:58:09 +0200

    Practica 7_3

---
+++ lesson7_3/src/Long_Float.java
@@ -0,0 +1,24 @@
+public class Long_Float {
+
+    static long long1 = 123456665657899874L;
+    static float float1 = 23.4F;
+    static char char1 = 5;
+
+    static int int1 = (int)long1;
+    static int int2;
+
+
+    public static void main(String[] args) {
+        // tre sa fie initializat obligatoriu
+        int int3=5;
+
+
+        System.out.println("Long number is: "+long1);
+        System.out.println("Long number int is: "+int1);
+        System.out.println("Int2: "+int2);
+        System.out.println("Int3: "+int3);
+
+
+    }
+
+}
